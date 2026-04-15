40d52e361756d9a46a002f00c787d04bc6f87b9d
Lorenzo SEKULES <l.sekules@ad.isae-supaero.fr>
2026-04-15 15:40:13 +0200

    Comparison 3

--- Assembling/Comparison.m
+++ Assembling/Comparison.m
@@ -8,21 +8,21 @@ Name_SIM = 'Robot3_SIMSCAPE';
 open(Name_SIM)
 G_SIMSCAPE_nominal=linearize(Name_SIM);
 
-M0=inv(dcgain(Gu(1:6,1:6)));
-M0_SIMSCAPE=inv(dcgain(G_SIMSCAPE_nominal(1:6,1:6)));
+M0=inv(dcgain(Gu(1:8,1:8)));
+M0_SIMSCAPE=inv(dcgain(G_SIMSCAPE_nominal(1:8,1:8)));
 IatB=M0(4:6,4:6);
 
 ComparisonDCgain=M0_SIMSCAPE-M0;
 disp(ComparisonDCgain)
 
 figure()
-sigma(Gum.NominalValue(1:6,1:6)-G_SIMSCAPE_nominal(1:6,1:6))
-norm(sigma(Gum.NominalValue(1:6,1:6)-G_SIMSCAPE_nominal(1:6,1:6)),'inf')
+sigma(Gum.NominalValue(1:8,1:8)-G_SIMSCAPE_nominal(1:8,1:8))
+norm(sigma(Gum.NominalValue(1:8,1:8)-G_SIMSCAPE_nominal(1:8,1:8)),'inf')
 
 %%
 figure()
-sigma(Gum.NominalValue(1:6,1:6))
+sigma(Gum.NominalValue(1:8,1:8))
 hold on
-sigma(G_SIMSCAPE_nominal(1:6,1:6))
+sigma(G_SIMSCAPE_nominal(1:8,1:8))
 
 legend('SDT','Simscape')
